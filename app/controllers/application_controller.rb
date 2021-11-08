533242f2d9ab57ba6bdda73daa1f43c41ffb60f5
francescoFH <francescofacca@gmail.com>
2021-11-08 11:44:31 +0000

    Feat: Handle Authentication error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,18 @@ class ApplicationController < ActionController::API
   )
   rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
 
+
+  rescue_from UserAuthenticator::AuthenticationError, with: :authenticator_error
+
+  private
+
+  def authenticator_error
+    error = {
+      status: "401",
+      source: { "pointer": "/code" },
+      title: "Authentication code is invalid",
+      detail: "You must provide valid code in order to exchange it for token."
+    }
+    render json: { "errors": [ error ] }, status: 401
+  end
 end
