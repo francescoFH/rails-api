7634f5dcfadeda3e15685467f9ec5aa30363230d
francescoFH <francescofacca@gmail.com>
2021-11-11 13:43:03 +0000

    Feat: Handle unauthorized request for logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::API
-  include JsonapiErrorsHandler
-
-  ErrorMapper.map_errors!(
-    'ActiveRecord::RecordNotFound' =>
-      'JsonapiErrorsHandler::Errors::NotFound'
-  )
-  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
-
-
-  rescue_from UserAuthenticator::AuthenticationError, with: :authenticator_error
+  class AuthorizationError < StandardError; end
+  # include JsonapiErrorsHandler
+  #
+  # ErrorMapper.map_errors!(
+  #   'ActiveRecord::RecordNotFound' =>
+  #     'JsonapiErrorsHandler::Errors::NotFound'
+  # )
+  # rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :authorization_error
 
   private
 
-  def authenticator_error
+  def authentication_error
     error = {
       status: "401",
       source: { "pointer": "/code" },
@@ -20,5 +20,15 @@ class ApplicationController < ActionController::API
       detail: "You must provide valid code in order to exchange it for token."
     }
     render json: { "errors": [ error ] }, status: 401
+  end
+
+  def authorization_error
+    error = {
+      status: "403",
+      source: { "pointer": "/headers/authorization" },
+      title: "Not authorized",
+      detail: "You have no right to access this resource."
+    }
+    render json: { "errors": [ error ] }, status: 403
   end
 end
